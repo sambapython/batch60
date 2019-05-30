bd5a48adfac26e0262c95e5fa1b1e1b28099d3a9
sambapython@gmail.com <sambapython@gmail.com>
2019-05-30 10:21:14 +0530

    2019-05-30 backup session.

--- app.py
+++ app.py
@@ -1,5 +1,9 @@
-print("hello")
 a=1000
 b=2000
-c=a+b
-print("result="c)
+c=a
+d=a+b
+print("result=",d)
+print(a,type(a),id(a))
+print(b, type(b),id(b))
+print(c, type(c),id(c))
+print(d, type(d),id(d))
